4700e7e11092bf4475e57f57a8cd1c106ab1d077
mimochikour <mohamed.khoualed@city.ac.uk>
2024-02-05 21:26:04 +0000

    Added 5 more platforms for the character tp jump on top of it, added a picture of rick

--- src/game/Game.java
+++ src/game/Game.java
@@ -31,15 +31,45 @@ public class Game {
         ground.setPosition(new Vec2(0f, -11.5f));
 
         // make a suspended platform
-        Shape platformShape = new BoxShape(3, 0.5f);
+        Shape platformShape = new BoxShape(1, 0.5f);
         StaticBody platform1 = new StaticBody(world, platformShape);
-        platform1.setPosition(new Vec2(-8, -4f));
+        platform1.setPosition(new Vec2(-8, -9f));
+
+        // make a second suspended platform
+        Shape platformShape1 = new BoxShape(1, 0.5f);
+        StaticBody platform2 = new StaticBody(world, platformShape1);
+        platform2.setPosition(new Vec2(-2, -6f));
+
+        // make a third suspended platform
+        Shape platformShape2 = new BoxShape(1, 0.5f);
+        StaticBody platform3 = new StaticBody(world, platformShape2);
+        platform3.setPosition(new Vec2(10, -3f));
+
+        // make a fourth suspended platform
+        Shape platformShape3 = new BoxShape(1, 0.5f);
+        StaticBody platform4 = new StaticBody(world, platformShape3);
+        platform4.setPosition(new Vec2(4, -1f));
+
+        // make a fifth suspended platform
+        Shape platformShape4 = new BoxShape(1, 0.5f);
+        StaticBody platform5 = new StaticBody(world, platformShape4);
+        platform5.setPosition(new Vec2(-8, 4f));
+
+        // make a sixth suspended platform
+        Shape platformShape5 = new BoxShape(1, 0.5f);
+        StaticBody platform6 = new StaticBody(world, platformShape5);
+        platform6.setPosition(new Vec2(-4, 6f));
+
+        // make a seventh suspended platform
+        Shape platformShape6 = new BoxShape(1, 0.5f);
+        StaticBody platform7 = new StaticBody(world, platformShape6);
+        platform7.setPosition(new Vec2(4, 6f));
 
         //make a character (with an overlaid image)
         Shape studentShape = new BoxShape(1,2);
         DynamicBody student = new DynamicBody(world, studentShape);
-        student.setPosition(new Vec2(4,-5));
-        student.addImage(new BodyImage("data/student.png", 4));
+        student.setPosition(new Vec2(5,-5));
+        student.addImage(new BodyImage("data/rickg.png", 4));
 
 
         //3. make a view to look into the game world
